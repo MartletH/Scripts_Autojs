2aaf3148db418043619bcabe090089e5ba545cac
Martlet <21070822+DarcyCJ@users.noreply.github.com>
2020-01-26 23:43:19 +0800

    a

--- 蚂蚁森林_GitHub.js
+++ 蚂蚁森林_GitHub.js
@@ -4,13 +4,3 @@
  * 20200126, Update desc to text, remove tasker functions (Not using)
   */
 
-/**
- * This will work for all interface as you open
- */
-
-textStartsWith("收集能量").find().forEach(function (t) {
-    log(t.text())
-    log(bnds = t.bounds())
-    log(click(bnds.centerX(), bnds.centerY()))
-})
-
